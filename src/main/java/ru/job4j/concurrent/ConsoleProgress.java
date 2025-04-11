bae26ed2412f8624115f92ff24bbf2c02628f938
kir <kirpro95@yandex.ru>
2025-04-11 11:23:34 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,25 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        while (!Thread.interrupted()) {
+            for (int i = 0; i < 4; i++) {
+            var process = new char[] {'-', '\\', '|', '/'};
+            System.out.print("\rload: " + process[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        progress.interrupt();
+    }
+}
