16d2f41e5a6357631889e384444dc7f0e87ed2f8
kir <kirpro95@yandex.ru>
2025-04-12 09:51:30 +0300

    4. Режим ожидания. [#231217 #539510]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -2,9 +2,16 @@ package ru.job4j.concurrent;
 
 public class Wget {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 101; i++) {
-            System.out.print("\rLoading : " + i + "%");
-            Thread.sleep(1000);
-        }
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 101; i++) {
+                System.out.print("\rLoading : " + i + "%");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        thread1.start();
     }
 }
