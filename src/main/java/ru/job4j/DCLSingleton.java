8b0c4a8f3659f422e888b3c8b031253aba1026f6
kir <kirpro95@yandex.ru>
2025-04-15 15:20:00 +0300

    2. Модель памяти Java [#267917]

---
+++ src/main/java/ru/job4j/DCLSingleton.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+public final class DCLSingleton {
+
+    private volatile static DCLSingleton instance;
+
+    public static DCLSingleton getInstance() {
+        if (instance == null) {
+            synchronized (DCLSingleton.class) {
+                if (instance == null) {
+                    instance = new DCLSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private DCLSingleton() {
+    }
+
+}
