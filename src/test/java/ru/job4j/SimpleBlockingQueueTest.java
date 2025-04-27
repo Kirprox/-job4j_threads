784cb95aa561de0856340f32c39bdc8667cbdac8
kir <kirpro95@yandex.ru>
2025-04-27 14:26:51 +0300

    1. Реализовать шаблон Producer Consumer. [#1098 #541457]

--- src/test/java/ru/job4j/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -14,31 +14,26 @@ class SimpleBlockingQueueTest {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         List<Integer> result = new ArrayList<>();
-
-        Thread producer = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int number : numbers) {
-                    try {
-                        queue.offer(number);
-                        result.add(number);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+        Thread consumer = new Thread(() -> {
+            while (queue.getSize() != 0) {
+                try {
+                    queue.poll();
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    break;
                 }
             }
         });
-        Thread consumer = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (queue.getSize() != 0) {
-                    try {
-                        queue.poll();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+        Thread producer = new Thread(() -> {
+            for (int number : numbers) {
+                try {
+                    queue.offer(number);
+                    result.add(number);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
+            consumer.interrupt();
         });
         producer.start();
         consumer.start();
